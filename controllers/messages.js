0e76346dfa25cbba5e951614e0a4b14ce04a98fa
Egor <egsrapps@gmail.com>
2024-09-08 00:03:50 +0200

    Send Message

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,12 +1,28 @@
 import { validateMessage } from "../schemes/message.js";
+import Message from '../models/message.js';
 
-const sendMessage = (req, res) => {
-    
+const sendMessage = async (req, res) => {
+    const {message} = req.body;
+
     const messageValidation = validateMessage(req.body);
 
     if(messageValidation.error){
         return res.status(422).json({message: messageValidation});
     }
+
+    try{
+        const completedMessage = Message({message: message});  
+        const savedMessage = await completedMessage.save();
+    
+        if(!savedMessage){
+            return res.status(500).json({message: "Error al crear el mensaje"});
+        }
+
+        return res.status(201).json({message: "Mensaje enviado"});
+    }catch(err){
+        return res.status(500).json({message: err.message});
+    }
+
 
 }
 
